07643bc4cd59be79b8bf7374aea9b6e57576d215
Wendesday36 <bertazoeanna@gmail.com>
2023-03-02 16:03:32 +0100

    3ciklus

--- SzamolosFeladatok/src/Ciklusok/Szamok.java
+++ SzamolosFeladatok/src/Ciklusok/Szamok.java
@@ -9,7 +9,22 @@ package Ciklusok;
  * @author berta
  */
 public class Szamok {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
+        szamokAlap();
+
+    }
+
+    private static void szamokAlap() {
+        for(int x =2; x < 3;x++){
+            for (int i = 2; i < 4; i++) {
+            for (int n = 2; n < 5; n++) {
+            System.out.println(x+" "+i+" "+n);
+
+            }
+
+        }
+        }
         
     }
 }
