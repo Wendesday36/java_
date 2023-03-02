e05e42b9e303c3e663a4db8cd7e490e56df20a00
Wendesday36 <bertazoeanna@gmail.com>
2023-03-02 16:42:39 +0100

    Sarkanyok

--- SzamolosFeladatok/src/Ciklusok/Szamok.java
+++ SzamolosFeladatok/src/Ciklusok/Szamok.java
@@ -12,19 +12,40 @@ public class Szamok {
 
     public static void main(String[] args) {
         szamokAlap();
-
+        beKeres();
     }
 
     private static void szamokAlap() {
-        for(int x =2; x < 3;x++){
+        for (int x = 2; x < 3; x++) {
             for (int i = 2; i < 4; i++) {
-            for (int n = 2; n < 5; n++) {
-            System.out.println(x+" "+i+" "+n);
+                for (int n = 2; n < 5; n++) {
+                    System.out.println(x + " " + i + " " + n);
+
+                }
 
             }
+        }
 
+    }
+
+    private static boolean beKeres() {
+        System.out.println("Ismétléssel vagy ismétlés nélkül szeretnéd?");
+        boolean False;
+        if (Boolean.FALSE) {
+            szamokAlap();
+        } 
+        else if(Boolean.TRUE) {
+            for (int x = 2; x < 3; x++) {
+                for (int i = 3; i < 4; i++) {
+                    for (int n = 2; n < 5; n++) {
+                        System.out.println(x + " " + i + " " + n);
+
+                    }
+
+                }
+            }
         }
-        }
-        
+        return false;
+   
     }
 }
---
+++ SzamolosFeladatok/src/sarkany/SarkanyFejek.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sarkany;
+
+/**
+ *
+ * @author berta
+ */
+public class SarkanyFejek {
+
+    public static void main(String[] args) {
+        for (int i = 7; i < 145; i++) {
+            for (int x = 11; x < 145; x++) {
+                System.out.print(i + " " + x);
+            }
+
+        }
+    }
+}
